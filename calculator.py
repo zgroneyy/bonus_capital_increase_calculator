ec9bb03e288f377fb370f79a37ef081a4f0b03d3
Özgür Öney <ozguroneyy@gmail.com>
2023-10-21 14:47:06 +0300

    Add GUI on calculator.py

--- calculator.py
+++ calculator.py
@@ -1,39 +1,57 @@
-# -*- coding: utf-8 -*-
-"""
-Calculate number of stocks needed in capital increase through bonus issues 
-in order to have "integer" number of stocks at the and of capital increase. 
+import tkinter as tk
 
->>Enter number of stocks you have
->>Get a list of 5 consist of # of stocks you need to have in order to have integer
-"""
-# Initial calculation was based on bonus capital increase of BIST.ISMEN, to be held on 2023
-# 322.53521% 
 def is_int(val):
     if type(val) == int:
-         return True
+        return True
     else:
-         if val.is_integer():
-             return True
-         else:
-             return False
-         
-lot = float(input("# of Stocks: "))
-bedelsiz_yuzde = 322.53521
+        if val.is_integer():
+            return True
+        else:
+            return False
 
-yeni_lot_sayisi = int(lot) * (bedelsiz_yuzde)/100
-print(yeni_lot_sayisi)
-decimal = str(yeni_lot_sayisi-int(yeni_lot_sayisi)).split('.')[1]
-print("You have 0." + str(decimal) + " as decimal")
+def calculate_stock_options():
+    lot = float(entry_lot.get())
+    capital_increase_percentage = float(entry_percentage.get())
 
-options_list = []
-checker = 0
-check=True
-while check:
-    if(is_int((lot + checker)*(bedelsiz_yuzde/100))):
-        options_list.append(checker)
-        if len(options_list) == 3:
-            check=False
-    checker+=1
-print(options_list)
+    yeni_lot_sayisi = int(lot) * (1 + capital_increase_percentage / 100)
+    decimal = yeni_lot_sayisi - int(yeni_lot_sayisi)
+    decimal_label.config(text=f"You have 0.{decimal:.6f} as decimal")
 
+    options_list = []
+    checker = 0
 
+    while len(options_list) < 3:
+        if is_int((lot + checker) * (1 + capital_increase_percentage / 100)):
+            options_list.append(checker)
+        checker += 1
+
+    options_label.config(text=f"Options: {options_list}")
+
+# Create the main window
+root = tk.Tk()
+root.title("Stock Calculator")
+
+# Create and place widgets in the window
+label_lot = tk.Label(root, text="Number of Stocks:")
+label_lot.pack()
+
+entry_lot = tk.Entry(root)
+entry_lot.pack()
+
+label_percentage = tk.Label(root, text="Percentage of Capital Increase:")
+label_percentage.pack()
+
+entry_percentage = tk.Entry(root)
+entry_percentage.pack()
+
+calculate_button = tk.Button(root, text="Calculate", command=calculate_stock_options)
+calculate_button.pack()
+
+decimal_label = tk.Label(root, text="")
+decimal_label.pack()
+
+options_label = tk.Label(root, text="")
+options_label.pack()
+
+# Start the GUI event loop
+root.mainloop()
