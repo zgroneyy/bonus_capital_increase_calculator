725063a4bd63dc39097d8e5ae73b444ce23fba37
Özgür Öney <ozguroneyy@gmail.com>
2023-10-21 14:54:45 +0300

    Update calculator.py
    Added "number of stocks you wish to have after capital increase"

--- calculator.py
+++ calculator.py
@@ -17,10 +17,17 @@ def calculate_stock_options():
     decimal = yeni_lot_sayisi - int(yeni_lot_sayisi)
     decimal_label.config(text=f"You have 0.{decimal:.6f} as decimal")
 
+    # Ask the user how many stocks they need to have after the capital increase
+    desired_stock_count = float(entry_desired_stock_count.get())
+
+    # Calculate the number of stocks that need to be bought before the capital increase
+    stocks_to_buy = max(0, int(desired_stock_count) - int(lot))
+    stocks_to_buy_label.config(text=f"Stocks to buy before capital increase: {stocks_to_buy}")
+
     options_list = []
     checker = 0
 
-    while len(options_list) < 5:  # Change the loop to find 5 options
+    while len(options_list) < 5:
         if is_int((lot + checker) * (1 + capital_increase_percentage / 100)):
             options_list.append(checker)
         checker += 1
@@ -43,11 +50,20 @@ label_percentage.pack()
 entry_percentage = tk.Entry(root)
 entry_percentage.pack()
 
+label_desired_stock_count = tk.Label(root, text="How many stocks you need to have after the capital increase?")
+label_desired_stock_count.pack()
+
+entry_desired_stock_count = tk.Entry(root)
+entry_desired_stock_count.pack()
+
 calculate_button = tk.Button(root, text="Calculate", command=calculate_stock_options)
 calculate_button.pack()
 
 decimal_label = tk.Label(root, text="")
 decimal_label.pack()
+
+stocks_to_buy_label = tk.Label(root, text="")
+stocks_to_buy_label.pack()
 
 options_label = tk.Label(root, text="")
 options_label.pack()
